a98327c6b422aeb7700dc52e21cffdbde4207a5a
intiv <intiv@unitec.edu>
2018-03-30 17:31:11 -0600

    Redirect > y >> funciona

--- cmd.cpp
+++ cmd.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <fstream>
 #include <string.h>
 #include <vector>
 #include <sys/wait.h>
@@ -37,29 +38,18 @@ int main() {
 	bool isRedirect;
 	bool piped = false;
 	vector<string> viktor;
-
-	// char** firstCommand;
-	// char** secondCommand;
-
 	while (1) {
 
 		isRedirect = false;
 		isPipe = false;
-		cout << ">";
 	   	comando = NULL;
 	   	param = NULL;
 	   	cin.clear();
 		fflush(stdin);
 	   	memset(linea, 0, sizeof(linea));
+		cout << ">";
 	   	cin.getline(linea, 50);
 	   	tempLinea = linea;
-
-	   	/*********************************
-	   	 *	INICIO PARTE DEL PIPE        *
-	   	 *********************************/
-
-	   	// cout << "linea "<<linea <<endl;
-
 	   	if (!tempLinea.empty()){
 	   		for (int i = 0; i < 50; ++i){
 		   		if (linea[i]  == '|'){
@@ -72,25 +62,16 @@ int main() {
 		   	}
 	   	}
 
-
 	   	if (isPipe){
 	   		piped =true;
-
 	   		string cmd1, cmd2;
 	   		char** firstCommand;
 			char** secondCommand;
 		   	cmd1 = strtok(linea, "|");
 		   	cmd2 = strtok(NULL, "");
-		   	// cout << "cmd1 : " << cmd1 <<endl;
-		   	// cout << "cmd2 : " << cmd2 <<endl;
 		   	firstCommand  = parseCommand2(cmd1);
 		   	secondCommand  = parseCommand2(cmd2);
-		   	// cout << "firstCommand[0]: " << firstCommand[0] << endl;
-		   	// cout << "firstCommand[1]: " << firstCommand[1] << endl;
-		   	// cout << "secondCommand[0]: " << secondCommand[0] << endl;
-		   	// cout << "secondCommand[1]: " << secondCommand[1] << endl;
 		   	piping(firstCommand, secondCommand);
-		   	// cout << "SALIO DE PIPE" << endl;
 
 		   	free(firstCommand[0]);
 		   	free(firstCommand[1]);
@@ -98,101 +79,19 @@ int main() {
 		   	free(secondCommand[1]);
 		   	free(firstCommand);
 		   	free(secondCommand);
-
-		   	// cout << "presione enter" <<endl;
-		   	// memset(linea, 0, sizeof(linea));
-	   		// cin.getline(linea, 50);
-
-			// string comandoCadena(linea);
-		   	// string delim = "|";
-		   	// parseCommand(comandoCadena, delim, &viktor, NULL);
-
-
-		   	// vector<string> parsed;
-		   	// vector<string> parsed2;
-		   	// delim = " ";
-		   	// //parseo de comandos
-		   	// parseCommand(viktor.at(0), delim, &parsed, NULL);
-		   	// parseCommand(viktor.at(1), delim, &parsed2, NULL);
-
-		   	// comandos = new char[parsed.at(0).length() + 1];
-		   	// strcpy(comandos, parsed.at(0).c_str());
-		   	// params = new char[parsed.at(1).length() + 1];
-		   	// strcpy(params, parsed.at(1).c_str());
-
-
-		   	// comandos2 = new char[parsed2.at(0).length() + 1];
-		   	// strcpy(comandos2, parsed2.at(0).c_str());
-		   	// params2 = new char[parsed2.at(1).length() + 1];
-		   	// strcpy(params2, parsed2.at(1).c_str());
-
-		   	// cout << comandos << endl;
-		   	// cout << params << endl;
-		   	// cout << "-------"<<endl;
-		   	// cout << comandos2 << endl;
-		   	// cout << params2 << endl;
-
-		   	//pipe
-
-		   	// int zelda[2];
-		   	// pid_t pid;
-		   	// char foo[4096];
-
-		   	// cout<<"-------------"<<endl;
-		   	// if(pipe(zelda) == -1)
-		   	// 	die("pipe");
-
-		   	// if ((pid = fork()) == -1)
-	    	// 	die("fork");
-
-	    	// if(pid == 0) {
-	    	// 	dup2 (zelda[1], STDOUT_FILENO);
-			   //  close(zelda[0]);
-			   //  close(zelda[1]);
-			   //  execlp(comandos, comandos, params, (char *)0);
-
-			   //  die("execlp");
-	    	// }else{
-	    	// 	close(zelda[1]);
-			   //  int nbytes = read(zelda[0], foo, sizeof(foo));
-			   //  printf("Output: (%.*s)\n", nbytes, foo);
-			   //  wait(NULL);
-			   //  execlp(comandos2, comandos2, foo, params2, (char*) 0);
-		   	// }
-    	/*********************************
-	   	 *	FIN PARTE DEL PIPE           *
-	   	 *********************************/
-
-	   	// for (int i = 0; i < viktor.size(); ++i)
-	   	// {
-	   	// 	if(i == 0)
-	   	// 		parseCommand(viktor.at(i), delim, NULL, &comandos);
-	   	// 	else
-	   	// 		parseCommand(viktor.at(i), delim, NULL, &comandos2);
-	   	// }
 	   	}else if(isRedirect){
-
 	   		char** commandArray = new char*[4];
-
 			char* commandRedirect;
 	   		char* argumentRedirect;
 	   		char* operatorType;
 	   		char* fileName;
-
 	   		commandRedirect =  strtok(linea, " ");
 	   		argumentRedirect =  strtok(NULL	, " ");
 	   		operatorType = strtok(NULL	, " ");
-
 	   		if (argumentRedirect[0] == '>' || argumentRedirect[0] == '2' ){
-
 	   			commandArray[0] = commandRedirect;
 	   			commandArray[1] = argumentRedirect;
 	   			commandArray[2] = operatorType;
-
-
-
-
-
 	   		}else {
 				fileName = strtok(NULL	, " ");
 	   			commandArray[0] = commandRedirect;
@@ -202,11 +101,6 @@ int main() {
 	   		}
 	   		//inicio de la funcion redirect
 	   		redirect(commandArray);
-
-
-
-
-
 	   		//liberando espacio
 	   		delete commandArray;
 
@@ -240,10 +134,8 @@ int main() {
 						if (strcmp(comando, "mi_pos") == 0) {
 							std::ostringstream oss;
 							comando = "cat";
-							cout << comando << endl;
 							string processDirectory = "/proc/";
 							string status = "/status";
-
 							int pid2 = getppid();
 							oss << pid2;
 							string processId = "";
@@ -251,17 +143,14 @@ int main() {
 							processDirectory += processId;
 							processDirectory += status;
 							param = const_cast<char*>(processDirectory.c_str());
-							execlp(comando, comando, param, NULL );
+							execlp(comando, comando, param, NULL);
 						} else {
-							cout << comando << endl;
-							cout << param << endl;
+							
 							execlp(comando, comando, param, NULL);
 						}
 			   	}else if((pid = fork())!=0){
 			   		wait(NULL);
 			   	}
-			}else {
-			   	// cout << "error prrin" <<endl;
 			}
 
 
@@ -307,17 +196,127 @@ void piping(char** cmd1, char** cmd2){
 		perror("execvp failed");
 
 		// parent process
-	} //else
-	    // waitpid(pid, NULL, 0);
+	} 
 }
 
 
-void redirect(char** commands){
+void redirect(char** commands){	
 	if (commands[3] == NULL){
-		cout<< "tamalo"<<endl;
-		//ejecutar redirect con el comando, operador y el archivo
+		char** command = new char*[1];
+		command[0] = strdup(commands[0]);
+		int cual;
+		if(!ifstream(commands[2])){
+			ofstream create(commands[2]);
+			create.close();
+		}
+		if(strcmp(commands[1], ">") == 0){
+
+			cual = 0;
+		}else if(strcmp(commands[1], ">>") == 0){
+			cual = 1;
+		}else if(strcmp(commands[1], ">&2") == 0){
+			cual = 2;
+
+		}else if(strcmp(commands[1], "2>&1") == 0){
+			cual = 3;
+		}
+		int fds[2];
+		pipe(fds);
+		char c;
+		int count;
+		pid_t pid;
+		char foo[4096];
+		if(fork() == 0){
+			dup2(fds[0], 0);
+			close(fds[1]);
+			int nbytes = read(fds[0], foo, sizeof(foo));
+    		ofstream escribir;
+    		if(cual == 0){
+    			escribir.open(commands[2], fstream::trunc);
+
+    		}else if(cual == 1){
+    			escribir.open(commands[2], fstream::app);
+
+    		}else if(cual == 2){
+    			escribir.open(commands[2], fstream::app);
+
+    		}else if(cual == 3){
+    			escribir.open(commands[2], fstream::app);
+
+    		}
+    		escribir << foo;
+    		escribir.close();
+    		execlp("echo", "echo", NULL);
+		}else if((pid = fork()) == 0){
+			dup2(fds[1], 1);
+			close(fds[0]);
+			execvp(command[0], command);
+		}else{
+			waitpid(pid, NULL, 0);
+			close(fds[0]);
+			close(fds[1]);
+			// fclose(archivo);
+		}
+		delete command;
 	}else{
-		cout<< "tabueno"<<endl;
+		char** command = new char*[2];
+		command[0] = strdup(commands[0]);
+		command[1] = strdup(commands[1]);
+
+		int cual;
+		if(!ifstream(commands[3])){
+			ofstream create(commands[3]);
+			create.close();
+		}
+		if(strcmp(commands[2], ">") == 0){
+
+			cual = 0;
+		}else if(strcmp(commands[2], ">>") == 0){
+			cual = 1;
+		}else if(strcmp(commands[2], ">&2") == 0){
+			cual = 2;
+
+		}else if(strcmp(commands[2], "2>&1") == 0){
+			cual = 3;
+		}
+		int fds[2];
+		pipe(fds);
+		char c;
+		int count;
+		pid_t pid;
+		char foo[4096];
+		if(fork() == 0){
+			dup2(fds[0], 0);
+			close(fds[1]);
+			int nbytes = read(fds[0], foo, sizeof(foo));
+    		ofstream escribir;
+    		if(cual == 0){
+    			escribir.open(commands[3], fstream::trunc);
+
+    		}else if(cual == 1){
+    			escribir.open(commands[3], fstream::app);
+
+    		}else if(cual == 2){
+    			escribir.open(commands[3], fstream::app);
+
+    		}else if(cual == 3){
+    			escribir.open(commands[3], fstream::app);
+
+    		}
+    		escribir << foo;
+    		escribir.close();
+    		execlp("echo", "echo", NULL);
+		}else if((pid = fork()) == 0){
+			dup2(fds[1], 1);
+			close(fds[0]);
+			execvp(command[0], command);
+		}else{
+			waitpid(pid, NULL, 0);
+			close(fds[0]);
+			close(fds[1]);
+			// fclose(archivo);
+		}
+		delete command;
 		//ejecutar redirect con el comando, argumento, operador y el archivo
 	}
 }
@@ -330,41 +329,20 @@ void parseCommand(string commandString, string delim, vector<string>* viktor, ch
 	string token;
 	char** retVal;
 	int cont = 0;
-	// if(delim != " "){
+   	while((pos = commandString.find(delim)) != string::npos){
+   		token = commandString.substr(0, pos);
+   		viktor->push_back(token);
+   		commandString.erase(0, pos + delim.length());
+   	}
+   	viktor->push_back(commandString);
 
-	   	while((pos = commandString.find(delim)) != string::npos){
-	   		token = commandString.substr(0, pos);
-	   		viktor->push_back(token);
-	   		commandString.erase(0, pos + delim.length());
-	   	}
-	   	viktor->push_back(commandString);
-	   	// return NULL;
-   // }else{
-   // 		cout << "Antes de empezar" <<endl;
-   // 		while((pos = commandString.find(delim)) != string::npos){
-	  //  		token = commandString.substr(0, pos);
-	  //  		// *(arreglo[cont]) = (char*) alloca(token.length() + 1);
-	  //  		// memcpy(*(arreglo[cont]), token.c_str(), token.size() + 1);
-	  //  		*(arreglo[cont]) = new char[token.length() + 1];
-	  //  		strcpy(*(arreglo[cont]), token.c_str());
-	  //  		cout << "Prueba" <<endl;
-	  //  		cont++;
-	  //  		commandString.erase(0, pos + delim.length());
-	  //  	}
-	  //  	cout << "Antes del ultimo"<<endl;
-	  //  	// *(arreglo[cont]) = (char*) alloca(commandString.size() + 1);
-	  //  	// memcpy(*(arreglo[cont]), commandString.c_str(), commandString.size() + 1);
-	  //  	// return &retVal;
-   // }
 }
 
 char** parseCommand2 (string buffer) {
 	vector<string> strings;
   	vector<char*> pointerChars;
-  	// string buffer;
   	string temporary;
-  	// getline(cin, buffer);
-  	// cout << "buffer: "<< buffer << endl;
+
   	for (size_t i = 0; i < buffer.length() + 1; i++) {
     	if (buffer[i] != ' ') {
       	temporary += buffer[i];
@@ -387,11 +365,4 @@ char** parseCommand2 (string buffer) {
 	    strcpy(sentences[i], strings.at(i).c_str());
   	}
   	return sentences;
-
-  	//LIBERANDO MEMORIA
-  	// for (size_t i = 0; i < strings.size(); i++) {
-   //  	cout << sentences[i] << endl;
-   //  	free(sentences[i]);
-  	// }
-  	// free(sentences);
 }
